ffcf2c8f6b55b17ac08d48087e8eb06d8a4d7101
YiboWang3813 <wyb103813@gmail.com>
2025-12-23 14:23:28 +0800

    添加了cov2D和inv_cov2D的计算

--- diff-gaussian-rasterization/include/covariance.cuh
+++ diff-gaussian-rasterization/include/covariance.cuh
@@ -55,3 +55,63 @@ void compute_cov3D(
     cov3D[4] = Sigma[5];
     cov3D[5] = Sigma[8];
 }
+
+
+__device__ __forceinline__
+void compute_cov2D(
+    float focal_x, float focal_y, 
+    float tan_fovx, float tan_fovy, 
+    const float* p_world_tilde, // [4]
+    const float* viewmatrix, // [16] 
+    const float* cov3D, // [6] 
+    float* cov2D // [3]
+)
+{
+    // compute p_view_tilde 
+    float p_view_tile[4]; 
+    mat4_mul_vec4_rowmajor(viewmatrix, p_world_tilde, p_view_tile); 
+
+    // constrain x and y component of p_view 
+    float limx = 1.3f * tan_fovx; 
+    float limy = 1.3f * tan_fovy; 
+    float xv_zv = p_view_tile[0] / p_view_tile[2]; 
+    float yv_zv = p_view_tile[1] / p_view_tile[2]; 
+    p_view_tile[0] = fminf(limx, fmaxf(-limx, xv_zv)) * p_view_tile[2]; 
+    p_view_tile[1] = fminf(limy, fmaxf(-limy, yv_zv)) * p_view_tile[2]; 
+    
+    // compute Jacobian J 
+    float J[9] = {0.0f}; 
+    J[0] = focal_x / p_view_tile[2]; 
+    J[2] = - (focal_x * p_view_tile[0]) / (p_view_tile[2] * p_view_tile[2]); 
+    J[4] = focal_y / p_view_tile[2]; 
+    J[5] = - (focal_y * p_view_tile[1]) / (p_view_tile[2] * p_view_tile[2]); 
+
+    // get rotation part of viewmatrix 
+    float Rview[9]; 
+    Rview[0] = viewmatrix[0]; Rview[1] = viewmatrix[1]; Rview[2] = viewmatrix[2]; 
+    Rview[3] = viewmatrix[4]; Rview[4] = viewmatrix[5]; Rview[5] = viewmatrix[6]; 
+    Rview[6] = viewmatrix[8]; Rview[7] = viewmatrix[9]; Rview[8] = viewmatrix[10]; 
+
+    // compute T = J * Rview 
+    float T[9]; 
+    mat3_mul_mat3_rowmajor(J, Rview, T); 
+
+    // complete full Sigma using cov3D 
+    float Sigma[9]; 
+    Sigma[0] = cov3D[0]; Sigma[1] = cov3D[1]; Sigma[2] = cov3D[2]; 
+    Sigma[3] = cov3D[1]; Sigma[4] = cov3D[3]; Sigma[5] = cov3D[4]; 
+    Sigma[6] = cov3D[2]; Sigma[7] = cov3D[4]; Sigma[8] = cov3D[5]; 
+
+    // compute Sigma2D = T * Sigma3D * T^T 
+    float Sigma2D[9]; 
+    float tmp[9]; 
+    mat3_mul_mat3_rowmajor(T, Sigma, tmp);
+    float T_trans[9]; 
+    mat3_transpose_rowmajor(T, T_trans); 
+    mat3_mul_mat3_rowmajor(tmp, T_trans, Sigma2D); 
+
+    // save 2D part of Sigma2D as cov2D 
+    cov2D[0] = Sigma2D[0]; 
+    cov2D[1] = Sigma2D[1]; 
+    cov2D[2] = Sigma2D[4]; 
+}
--- diff-gaussian-rasterization/src/forward.cu
+++ diff-gaussian-rasterization/src/forward.cu
@@ -10,6 +10,12 @@ namespace cg = cooperative_groups;
 
 __global__ void kernel_preprocess(
     int n_gauss, 
+    // params 
+    const float focal_x, 
+    const float focal_y, 
+    const float tan_fovx, 
+    const float tan_fovy, 
+    // gaussian attributes 
     const float* orig_points, 
     const float* scales, 
     const float* quats, 
@@ -18,7 +24,9 @@ __global__ void kernel_preprocess(
     // output 
     int* radii, 
     uint32_t* tile_touched, 
-    bool prefiltered
+    // control 
+    bool prefiltered, 
+    bool antialiasing
 )
 {
     auto idx = cg::this_grid().thread_rank(); 
@@ -43,19 +51,47 @@ __global__ void kernel_preprocess(
     float p_clip[4]; 
     mat4_mul_vec4_rowmajor(projmatrix, p_view_tilde, p_clip); 
     float inv_w = 1.0f / (p_clip[3] + 1e-6f); 
-    float p_ndc[3] = {
-        p_clip[0] * inv_w, 
-        p_clip[1] * inv_w, 
-        p_clip[2] * inv_w 
-    };
+    float p_ndc[3] = {p_clip[0] * inv_w, p_clip[1] * inv_w, p_clip[2] * inv_w};
 
     // compute cov3D always in CUDA 
     const float* scale = &scales[3 * idx]; 
     const float* quat = &quats[4 * idx]; 
     float cov3D[6]; 
-    compute_cov3D(scale, quat, cov3D);  
+    compute_cov3D(scale, quat, cov3D); 
 
+    // compute cov2D 
+    float cov2D[3]; 
+    compute_cov2D(
+        focal_x, focal_y, tan_fovx, tan_fovy, 
+        p_world_tilde, viewmatrix, cov3D, cov2D 
+    ); 
 
+    // get conic (inverse cov2D) under the antialiasing mode 
+    float opacity_scale = 1.0f; 
+
+    float cov2D_filtered[3] = {cov2D[0], cov2D[1], cov2D[2]}; 
+
+    if (antialiasing)
+    {
+        const float h_var = 0.3f; 
+        cov2D_filtered[0] += h_var; 
+        cov2D_filtered[2] += h_var; 
+
+        float det_cov2D = cov2D[0] * cov2D[2] 
+                        - cov2D[1] * cov2D[1]; 
+        float det_cov2D_filtered = cov2D_filtered[0] * cov2D_filtered[2] 
+                                - cov2D_filtered[1] * cov2D_filtered[1];
+
+        opacity_scale = sqrt(max(2.5e-5f, det_cov2D / det_cov2D_filtered)); 
+    }
+
+    float conic[3]; 
+    float det_cov2D_filtered = cov2D_filtered[0] * cov2D_filtered[2] 
+                            - cov2D_filtered[1] * cov2D_filtered[1];
+    float inv_det_filtered = 1.0f / det_cov2D_filtered; 
+    conic[0] = inv_det_filtered * cov2D_filtered[2]; 
+    conic[1] = -inv_det_filtered * cov2D_filtered[1]; 
+    conic[2] = inv_det_filtered * cov2D_filtered[0]; 
 
     
 }
